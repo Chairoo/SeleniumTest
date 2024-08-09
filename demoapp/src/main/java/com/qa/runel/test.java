7d97ecca4791e54af8dd80461a6882c5b5002071
AyelChairo <ruielchairod@gmail.com>
2024-08-09 12:40:16 +0800

    Added thread.sleep on test.java

--- demoapp/src/main/java/com/qa/runel/test.java
+++ demoapp/src/main/java/com/qa/runel/test.java
@@ -6,10 +6,11 @@ import org.openqa.selenium.edge.EdgeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.edgedriver().setup();
         WebDriver driver = new EdgeDriver();
         driver.get("https://www.facebook.com");
+        Thread.sleep(1000);
         driver.quit();
 
     }
