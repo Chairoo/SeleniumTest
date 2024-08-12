2b2caf700f781e7ed0003e15dc8c4fc79902e81e
AyelChairo <ruielchairod@gmail.com>
2024-08-12 09:25:57 +0800

    Added code in JavaNiRuiel

--- demoapp/src/main/java/com/qa/runel/JavaNiRuiel.java
+++ demoapp/src/main/java/com/qa/runel/JavaNiRuiel.java
@@ -2,6 +2,11 @@ package com.qa.runel;
 
 public class JavaNiRuiel {
     public static void main(String[] args) {
-        //Hello, test for pull request senpai inamernz
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.facebook.com");
+        Thread.sleep(1000);
+        driver.quit();
+        
     }
 }
