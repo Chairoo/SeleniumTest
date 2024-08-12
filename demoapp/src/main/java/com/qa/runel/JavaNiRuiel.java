ef308d8caf80fb4b6e5e857fcbf6ede4111523c9
AyelChairo <ruielchairod@gmail.com>
2024-08-12 09:35:36 +0800

    Changes in JavaNiRuiel

--- demoapp/src/main/java/com/qa/runel/JavaNiRuiel.java
+++ demoapp/src/main/java/com/qa/runel/JavaNiRuiel.java
@@ -1,12 +1,19 @@
 package com.qa.runel;
 
-public class JavaNiRuiel {
-    public static void main(String[] args) {
-        WebDriverManager.edgedriver().setup();
-        WebDriver driver = new EdgeDriver();
-        driver.get("https://www.facebook.com");
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class JavaNiRuiel{
+    public static void main(String[] args) throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/form");
+        
         Thread.sleep(1000);
         driver.quit();
-        
+
     }
 }
