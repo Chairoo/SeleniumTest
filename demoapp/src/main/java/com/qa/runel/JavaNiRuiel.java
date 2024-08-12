3ec36deee147c9f0f594e840bbaa715384e7f9be
Chairoo <dacpanorunel@gmail.com>
2024-08-12 09:32:12 +0800

    Updated java ni JavaNiRuiel

--- demoapp/src/main/java/com/qa/runel/JavaNiRuiel.java
+++ demoapp/src/main/java/com/qa/runel/JavaNiRuiel.java
@@ -1,12 +1,17 @@
 package com.qa.runel;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class JavaNiRuiel {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.edgedriver().setup();
         WebDriver driver = new EdgeDriver();
         driver.get("https://www.facebook.com");
         Thread.sleep(1000);
         driver.quit();
-        
+
     }
 }
